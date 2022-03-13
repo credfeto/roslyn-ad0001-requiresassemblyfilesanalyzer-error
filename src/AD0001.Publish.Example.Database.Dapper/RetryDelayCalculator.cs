0a93a6cc1802633d4988fc652c2921ab107391a4
Mark Ridgwell <credfeto@users.noreply.github.com>
2022-03-13 13:15:59 +0000

    Removing more code

--- src/AD0001.Publish.Example.Database.Dapper/RetryDelayCalculator.cs
+++ src/AD0001.Publish.Example.Database.Dapper/RetryDelayCalculator.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics.CodeAnalysis;
 
 namespace AD0001.Publish.Example.Database.Dapper;
 
@@ -8,8 +7,6 @@ namespace AD0001.Publish.Example.Database.Dapper;
 /// </summary>
 internal static class RetryDelayCalculator
 {
-    private static readonly Random RandomNumberGenerator = new();
-
     /// <summary>
     ///     Calculate the retry delay based on the current number of attempts
     /// </summary>
@@ -23,44 +20,8 @@ internal static class RetryDelayCalculator
             : TimeSpan.FromSeconds(CalculateBackoff(attempts));
     }
 
-    /// <summary>
-    ///     Calculate the retry delay based on the current number of attempts
-    /// </summary>
-    /// <param name="attempts">The number of attempts</param>
-    /// <param name="maxJitterSeconds">The maximum number of seconds for jitter.</param>
-    /// <returns>The time to wait before the next attempt</returns>
-    public static TimeSpan CalculateWithJitter(int attempts, int maxJitterSeconds)
-    {
-        // do a fast first retry, then exponential backoff
-        return attempts <= 1
-            ? TimeSpan.Zero
-            : TimeSpan.FromSeconds(WithJitter(CalculateBackoff(attempts), maxSeconds: maxJitterSeconds));
-    }
-
     private static double CalculateBackoff(int attempts)
     {
         return Math.Pow(x: 2, y: attempts);
-    }
-
-    private static double WithJitter(double delaySeconds, int maxSeconds)
-    {
-        double nonJitterPeriod = delaySeconds - maxSeconds;
-        double jitterRange = maxSeconds * 2;
-
-        if (nonJitterPeriod < 0)
-        {
-            jitterRange = delaySeconds;
-            nonJitterPeriod = delaySeconds / 2;
-        }
-
-        double jitter = CalculateJitterSeconds(jitterRange);
-
-        return nonJitterPeriod + jitter;
-    }
-
-    [SuppressMessage(category: "Microsoft.Security", checkId: "CA5394:Do not use insecure randomness", Justification = "Just a re-try delay")]
-    private static double CalculateJitterSeconds(double jitterRange)
-    {
-        return jitterRange * RandomNumberGenerator.NextDouble();
     }
 }
