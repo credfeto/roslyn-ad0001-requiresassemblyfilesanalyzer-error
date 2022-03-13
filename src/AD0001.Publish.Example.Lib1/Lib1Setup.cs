97d0268ed4432e8d5cb8f7cd5977b34023de669d
Mark Ridgwell <credfeto@users.noreply.github.com>
2022-03-13 12:52:36 +0000

    Missing files

---
+++ src/AD0001.Publish.Example.Lib1/Lib1Setup.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace AD0001.Publish.Example.Lib1;
+
+public static class Lib1Setup
+{
+    public static IServiceCollection AddTwitch(this IServiceCollection services)
+    {
+        return services;
+    }
+}
